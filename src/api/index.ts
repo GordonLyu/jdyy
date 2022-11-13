32b1a916d1afaa16c275f4966d84bc0929920b3d
cuanzi <1026551395@qq.com>
2022-11-13 21:55:42 +0800

    登录请求完成部分

---
+++ src/api/index.ts
@@ -0,0 +1,5 @@
+// 当前文件是对API进行统一管理
+import requests from '../utils/requests'
+
+
+// 登录
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -5,8 +5,8 @@
                 <el-form ref="ruleFormRef" :model="ruleForm" status-icon :rules="rules" label-width="0"
                     class="demo-ruleForm">
                     <!-- 用户名 -->
-                    <el-form-item prop="userName">
-                        <el-input v-model="ruleForm.userName" class="w-50 m-3" size="large" placeholder="用户名">
+                    <el-form-item prop="username">
+                        <el-input v-model="ruleForm.username" class="w-50 m-3" size="large" placeholder="用户名">
                             <template #prefix>
                                 <el-icon class="el-input__icon">
                                     <User />
@@ -54,16 +54,17 @@ import type { FormInstance } from 'element-plus'
 import card from "@/components/card/card.vue";
 import { User, Lock } from "@element-plus/icons-vue";
 import { ElMessage } from "element-plus";
-
+import { doLogin } from '@/api/login';
+import qs from "qs";
 const ruleFormRef = ref<FormInstance>()
 
 const ruleForm = reactive({
-    userName: '',
+    username: '',
     password: '',
 })
 
 const rules = reactive({
-    userName: [
+    username: [
         {
             required: true,
             message: "用户名不能为空",
@@ -78,7 +79,18 @@ const submitForm = (formEl: FormInstance | undefined) => {
     if (!formEl) return
     formEl.validate((valid) => {
         if (valid) {
-           
+            console.log(ruleForm);
+            
+           doLogin(qs.stringify(ruleForm)).then(res=>{
+            console.log(res.code);
+            if(res.code===200){
+                ElMessage.success("登录成功")
+            }else if(res.code===404){
+                ElMessage.error("用户不存在")
+            }else{
+                ElMessage.error("登录失败")
+            }
+           })
         } else {
             ElMessage.error("用户名和密码不能为空")
             return false
@@ -90,6 +102,21 @@ const resetForm = (formEl: FormInstance | undefined) => {
     if (!formEl) return
     formEl.resetFields()
 }
+
+// function sendData() :void{
+//          let that: object=this;
+//           this.$axios({
+//             method:'get',
+//             url:'http://127.0.0.1:8000/v3baiuserlogin/',
+//             params:{
+//              userName:this.userName,  //2.1需要给后端传的俩参数
+//              pwd:this.pwd
+//             }
+//             }).then(function(data){
+//                that.returnarr=data.data  //3.returnarr接收后端返回的参数
+//                that.judgedata()    //4.接受完参数就可以在这判断成功与否
+//             });
+//         }
 </script>
 
 <style scoped>
