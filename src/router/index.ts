4c117af09daac40fb7c3cdf15451f7d27ceddf90
LvXinming <1533159440@qq.com>
2022-12-01 20:56:02 +0800

    :sparkles: 添加一个音乐后台管理页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import BackstageView from '@/views/backstage/Backstage.vue'
+import AddUser from '@/views/backstage/components/Main/AddUser.vue'
+import AddMusic from '@/views/backstage/components/Main/AddMusic.vue'
 import Users from '@/views/backstage/components/Main/Users.vue'
+import Music from '@/views/backstage/components/Main/Music.vue'
+import Test from '@/views/backstage/components/Main/Test.vue'
+
 import adminLogin from '@/views/login/Login.vue'
 import Register from '@/views/register/Register.vue'
-import Music from '@/views/backstage/components/Main/Music.vue'
 import musicFrout from '@/views/fronts/musicFrout.vue'
 import musicPlay from '@/views/fronts/musicPlay/musicPlay.vue'
 import musicLIst from '@/views/fronts/musicList/musicLIst.vue'
@@ -11,6 +15,8 @@ import musicLIst from '@/views/fronts/musicList/musicLIst.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+
+    //后台管理
     {
       path: '/admin',
       name: 'backstage',
@@ -20,24 +26,46 @@ const router = createRouter({
       component: BackstageView,
       children:[
         {
-          path: '/users',
+          path: 'addUser',
+          name: 'addUser',
+          meta:{
+            title:"添加用户"
+          },
+          component: AddUser
+        },{
+          path: 'addMusic',
+          name: 'addMusic',
+          meta:{
+            title:"添加音乐"
+          },
+          component: AddMusic
+        },{
+          path: 'users',
           name: 'users',
           meta:{
-            title:"后台管理系统--用户表"
+            title:"用户表"
           },
           component: Users
-        },
-        {
-          path: '/music',
+        },{
+          path: 'music',
           name: 'music',
           meta:{
-            title:"后台管理系统--音乐表"
+            title:"音乐表"
           },
           component: Music
+        },{
+          path: 'test',
+          name: 'test',
+          meta:{
+            title:"测试"
+          },
+          component: Test
         },
 
       ]
     },
+
+    //前台
     {
       path:'/musicFrout',
       name:'musicFrout',
@@ -53,11 +81,15 @@ const router = createRouter({
         },
       ]
     },
+
+    //登录
     {
       path:'/login',
       name:'adminLogin',
       component:adminLogin,
     },
+
+    //注册
     {
       path:'/register',
       name:'Register',
